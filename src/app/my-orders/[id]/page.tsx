0821ac540287e866634069d712cf2b60dfe59a54
dharanigovardhan2008 <palerugopi00@gmail.com>
2026-09-22 20:47:06 +0530

    Fix order detail client component

--- src/app/my-orders/[id]/page.tsx
+++ src/app/my-orders/[id]/page.tsx
@@ -4,7 +4,10 @@ interface OrderDetailPageProps {
   params: Promise<{ id: string }>;
 }
 
-export default async function OrderDetailPage({ params }: OrderDetailPageProps) {
+export default async function OrderDetailPage({
+  params,
+}: OrderDetailPageProps) {
   const { id } = await params;
+
   return <OrderDetailClient orderId={id} />;
 }
